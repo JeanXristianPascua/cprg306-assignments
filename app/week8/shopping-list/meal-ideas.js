5c90441f38694b0634b83cbe38cb8bbda0a958dc
JeanXristianPascua <119911259+JeanXristianPascua@users.noreply.github.com>
2023-11-06 05:40:15 -0700

    Add Code

--- app/week8/shopping-list/meal-ideas.js
+++ app/week8/shopping-list/meal-ideas.js
@@ -2,55 +2,93 @@
 
 import React, { useState, useEffect } from 'react';
 
-async function fetchMealIdeas(ingredient) {
-    try {
-      const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
-      const data = await response.json();
-      return data.meals;
-    } catch (error) {
-      console.error(error);
+async function fetchMealIngredients(mealName) {
+  try {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`);
+    const data = await response.json();
+    if (data && data.meals && data.meals[0]) {
+      const meal = data.meals[0];
+      const ingredients = [];
+      for (let i = 1; i <= 20; i++) {
+        if (meal[`strIngredient${i}`]) {
+          ingredients.push(`${meal[`strMeasure${i}`]} ${meal[`strIngredient${i}`]}`);
+        } else {
+          break;
+        }
+      }
+      return ingredients;
     }
-  };
+    return [];
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+async function fetchMealIdeas(ingredient) {
+  try {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
+    const data = await response.json();
+    if (data && data.meals) {
+      return data.meals.map((meal) => ({
+        name: meal.strMeal,
+        id: meal.idMeal,
+        image: meal.strMealThumb,
+      }));
+    }
+    return [];
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
 
 export default function MealIdeas({ ingredient }) {
-  const [meals, setMeals] = useState([]);
-  const [selectedMealImage, setSelectedMealImage] = useState(null);
-  const [selectedMealId, setSelectedMealId] = useState(null);
-
+  const [mealIdeas, setMealIdeas] = useState([]);
+  const [selectedMeal, setSelectedMeal] = useState(null);
 
   async function loadMealIdeas() {
-    const fetchedMeals = await fetchMealIdeas(ingredient);
-    setMeals(fetchedMeals);
-  };
-  
+    const fetchedMealIdeas = await fetchMealIdeas(ingredient);
+    setMealIdeas(fetchedMealIdeas);
+  }
+
   useEffect(() => {
     loadMealIdeas();
   }, [ingredient]);
 
-  const handleMealClick = (meal) => {
-    if (selectedMealId === meal.idMeal) {
-      setSelectedMealId(null);
-      setSelectedMealImage(null);
+  const handleMealClick = async (meal) => {
+    if (selectedMeal && selectedMeal.id === meal.id) {
+      setSelectedMeal(null);
     } else {
-      setSelectedMealId(meal.idMeal);
-      setSelectedMealImage(meal.strMealThumb);
+      const ingredients = await fetchMealIngredients(meal.name);
+      setSelectedMeal({ ...meal, ingredients });
     }
   };
 
   const renderMeals = () => {
-    if (meals && meals.length > 0) {
+    if (mealIdeas && mealIdeas.length > 0) {
       return (
         <div>
           <p>Meal Ideas with {ingredient}:</p>
           <ul>
-            {meals && meals.map((meal) => (
-              <li className='p-2 m-1 bg-slate-900 max-w-sm hover:bg-orange-800 cursor-pointer' 
-                  key={meal.idMeal}
-                  onClick={() => handleMealClick(meal)}>
-                {meal.strMeal}
-                <div>
-                  {selectedMealId === meal.idMeal && selectedMealImage && <img src={selectedMealImage} alt={meal.strMeal} />}
-                </div>
+            {mealIdeas.map((meal) => (
+              <li
+                className="p-2 m-1 bg-slate-900 max-w-sm hover:bg-orange-800 cursor-pointer"
+                key={meal.id}
+                onClick={() => handleMealClick(meal)}
+              >
+                {meal.name}
+                {selectedMeal && selectedMeal.id === meal.id && (
+                  <div>
+                    <img src={meal.image} alt={meal.name} />
+                    <p>Ingredients:</p>
+                    <ul>
+                      {selectedMeal.ingredients.map((ingredient, index) => (
+                        <li key={index}>{ingredient}</li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
               </li>
             ))}
           </ul>
